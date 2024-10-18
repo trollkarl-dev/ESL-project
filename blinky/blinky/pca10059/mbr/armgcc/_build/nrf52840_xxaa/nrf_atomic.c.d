e43a5c40208e883f56c82c9be315a7c6f7de0c17
trollkarl-dev <machnev.dmitry@gmail.com>
2024-10-18 13:22:01 +0300

    Move the content of the blinky sample into your project directory
    Related to #1

---
+++ blinky/blinky/pca10059/mbr/armgcc/_build/nrf52840_xxaa/nrf_atomic.c.d
@@ -0,0 +1,94 @@
+_build/nrf52840_xxaa/nrf_atomic.c.o: \
+ ../../../../../../components/libraries/atomic/nrf_atomic.c \
+ ../../../../../../components/libraries/atomic/nrf_atomic.h \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ /usr/lib/gcc/arm-none-eabi/14.1.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/14.1.0/include/stdbool.h \
+ /usr/arm-none-eabi/include/string.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/lib/gcc/arm-none-eabi/14.1.0/include/stddef.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/string.h ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/softdevice/mbr/headers/nrf_mbr.h \
+ ../../../../../../components/softdevice/mbr/headers/nrf_svc.h \
+ ../../../../../../components/libraries/util/sdk_macros.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/atomic/nrf_atomic_internal.h
+../../../../../../components/libraries/atomic/nrf_atomic.h:
+../../../../../../components/libraries/util/sdk_common.h:
+/usr/lib/gcc/arm-none-eabi/14.1.0/include/stdint.h:
+/usr/arm-none-eabi/include/stdint.h:
+/usr/arm-none-eabi/include/machine/_default_types.h:
+/usr/arm-none-eabi/include/sys/features.h:
+/usr/arm-none-eabi/include/_newlib_version.h:
+/usr/arm-none-eabi/include/sys/_intsup.h:
+/usr/arm-none-eabi/include/sys/_stdint.h:
+/usr/lib/gcc/arm-none-eabi/14.1.0/include/stdbool.h:
+/usr/arm-none-eabi/include/string.h:
+/usr/arm-none-eabi/include/_ansi.h:
+/usr/arm-none-eabi/include/newlib.h:
+/usr/arm-none-eabi/include/sys/config.h:
+/usr/arm-none-eabi/include/machine/ieeefp.h:
+/usr/arm-none-eabi/include/sys/reent.h:
+/usr/arm-none-eabi/include/_ansi.h:
+/usr/lib/gcc/arm-none-eabi/14.1.0/include/stddef.h:
+/usr/arm-none-eabi/include/sys/cdefs.h:
+/usr/arm-none-eabi/include/sys/_types.h:
+/usr/arm-none-eabi/include/machine/_types.h:
+/usr/arm-none-eabi/include/sys/lock.h:
+/usr/arm-none-eabi/include/sys/string.h:
+../config/sdk_config.h:
+../../../../../../components/libraries/util/nordic_common.h:
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+../../../../../../components/libraries/util/sdk_os.h:
+../../../../../../components/libraries/util/sdk_errors.h:
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+../../../../../../components/libraries/util/app_util.h:
+../../../../../../modules/nrfx/mdk/nrf.h:
+../../../../../../modules/nrfx/mdk/nrf52840.h:
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+../../../../../../modules/nrfx/mdk/system_nrf52840.h:
+../../../../../../modules/nrfx/mdk/nrf52840_bitfields.h:
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52840.h:
+../../../../../../modules/nrfx/mdk/nrf52_to_nrf52840.h:
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+../../../../../../components/softdevice/mbr/headers/nrf_mbr.h:
+../../../../../../components/softdevice/mbr/headers/nrf_svc.h:
+../../../../../../components/libraries/util/sdk_macros.h:
+../../../../../../components/libraries/util/nrf_assert.h:
+../../../../../../components/libraries/atomic/nrf_atomic_internal.h:
