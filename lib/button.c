2ad3c09b3329ba740d1067bd1d4a4c5b47434e66
trollkarl-dev <machnev.dmitry@gmail.com>
2024-11-01 16:57:24 +0300

    Update button handling library (v2)

--- lib/button.c
+++ lib/button.c
@@ -8,25 +8,43 @@ void button_init(struct button *b)
     b->pressed_flag = 0;
 }
 
+static bool button_is_rising(struct button *b)
+{
+    return !b->pressed_flag && b->shift_reg == b_shift_reg_top;
+}
+
+static bool button_is_falling(struct button *b)
+{
+    return b->pressed_flag && b->shift_reg == b_shift_reg_down;
+}
+
+static bool button_is_released(struct button *b)
+{
+    return b->prev_timer == 0 && !b->pressed_flag && b->click_counter != 0;
+}
+
 void button_check(struct button *b)
 {
     b->shift_reg = (b->shift_reg << 1) | b->is_pressed();
-    b->prev_timer = (b->prev_timer != 0) ? b->prev_timer - 1
-                                         : 0;
 
-    if (!b->pressed_flag && b->shift_reg == b_shift_reg_top)
+    if (b->prev_timer != 0)
+    {
+        b->prev_timer--;
+    }
+
+    if (button_is_rising(b))
     {
         b->pressed_flag = true;
         b->click_counter++;
     }
 
-    if (b->pressed_flag && b->shift_reg == b_shift_reg_down)
+    if (button_is_falling(b))
     {
         b->pressed_flag = false;
         b->prev_timer = b_prev_timer_top;
     }
 
-    if (b->prev_timer == 0 && !b->pressed_flag && b->click_counter != 0)
+    if (button_is_released(b))
     {
         b->callback(b->click_counter);
         b->click_counter = 0;
