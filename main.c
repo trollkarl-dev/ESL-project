42a14f2142e88e5794241b74a49632215c7da800
trollkarl-dev <machnev.dmitry@gmail.com>
2024-11-23 10:46:56 +0300

    Merge main project with logs_and_cdc_acm example code
    Related to #11

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 
 #include "app_usbd.h"
 #include "app_usbd_serial_num.h"
+#include "app_usbd_cdc_acm.h"
 
 #include "nrf_bootloader_info.h"
 #include "nrf_dfu_types.h"
@@ -355,6 +356,89 @@ static void save_state(void)
                      : "Failed to save the selected color");
 }
 
+#define READ_SIZE 1
+
+static char m_rx_buffer[READ_SIZE];
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event);
+
+/* Make sure that they don't intersect with LOG_BACKEND_USB_CDC_ACM */
+#define CDC_ACM_COMM_INTERFACE  2
+#define CDC_ACM_COMM_EPIN       NRF_DRV_USBD_EPIN3
+
+#define CDC_ACM_DATA_INTERFACE  3
+#define CDC_ACM_DATA_EPIN       NRF_DRV_USBD_EPIN4
+#define CDC_ACM_DATA_EPOUT      NRF_DRV_USBD_EPOUT4
+
+APP_USBD_CDC_ACM_GLOBAL_DEF(usb_cdc_acm,
+                            usb_ev_handler,
+                            CDC_ACM_COMM_INTERFACE,
+                            CDC_ACM_DATA_INTERFACE,
+                            CDC_ACM_COMM_EPIN,
+                            CDC_ACM_DATA_EPIN,
+                            CDC_ACM_DATA_EPOUT,
+                            APP_USBD_CDC_COMM_PROTOCOL_NONE);
+
+
+static void usb_ev_handler(app_usbd_class_inst_t const * p_inst,
+                           app_usbd_cdc_acm_user_event_t event)
+{
+    switch (event)
+    {
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_OPEN:
+    {
+        ret_code_t ret;
+        ret = app_usbd_cdc_acm_read(&usb_cdc_acm, m_rx_buffer, READ_SIZE);
+        UNUSED_VARIABLE(ret);
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_PORT_CLOSE:
+    {
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_TX_DONE:
+    {
+        NRF_LOG_INFO("tx done");
+        break;
+    }
+    case APP_USBD_CDC_ACM_USER_EVT_RX_DONE:
+    {
+        ret_code_t ret;
+        do
+        {
+            /*Get amount of data transfered*/
+            size_t size = app_usbd_cdc_acm_rx_size(&usb_cdc_acm);
+            NRF_LOG_INFO("rx size: %d", size);
+
+            /* It's the simple version of an echo. Note that writing doesn't
+             * block execution, and if we have a lot of characters to read and
+             * write, some characters can be missed.
+             */
+            if (m_rx_buffer[0] == '\r' || m_rx_buffer[0] == '\n')
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm, "\r\n", 2);
+            }
+            else
+            {
+                ret = app_usbd_cdc_acm_write(&usb_cdc_acm,
+                                             m_rx_buffer,
+                                             READ_SIZE);
+            }
+
+            /* Fetch data until internal buffer is empty */
+            ret = app_usbd_cdc_acm_read(&usb_cdc_acm,
+                                        m_rx_buffer,
+                                        READ_SIZE);
+        } while (ret == NRF_SUCCESS);
+
+        break;
+    }
+    default:
+        break;
+    }
+}
+
 int main(void)
 {
     void *cpicker_saved_state;
@@ -369,11 +453,16 @@ int main(void)
     nrf_drv_clock_init();
     nrf_drv_clock_lfclk_request(NULL);
 
+
     pwm_init(&my_pwm, channels, max_pct, true);
     pwm_run(&my_pwm);
 
     logs_init();
     NRF_LOG_INFO("Starting up the HSV color-picker device");
+
+    app_usbd_class_inst_t const * class_cdc_acm = app_usbd_cdc_acm_class_inst_get(&usb_cdc_acm);
+    ret_code_t ret = app_usbd_class_append(class_cdc_acm);
+    APP_ERROR_CHECK(ret);
 
     app_timer_init();
     create_timers();
@@ -408,6 +497,11 @@ int main(void)
 
     while (true)
     {
+        while (app_usbd_event_queue_process())
+        {
+            /* Nothing to do */
+        }
+
         LOG_BACKEND_USB_PROCESS();
         NRF_LOG_PROCESS();
 
